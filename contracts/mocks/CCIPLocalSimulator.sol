2c10fd2706ee6b1f73036870339a7cd2cd18142d
elvis.he <elvis.he@gaiaworks.cn>
2025-07-19 16:28:23 +0800

    mock

--- contracts/mocks/CCIPLocalSimulator.sol
+++ contracts/mocks/CCIPLocalSimulator.sol
@@ -3,24 +3,3 @@
 pragma solidity ^0.8.28;
 
 import {CCIPLocalSimulator} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
-
-contract CCIPLocalSimulator is Test {
-    CCIPLocalSimulator public ccipLocalSimulator;
-
-    function setUp() public {
-        ccipLocalSimulator = new CCIPLocalSimulator();
-        (
-            uint64 chainSelector,
-            IRouterClient sourceRouter,
-            IRouterClient destinationRouter,
-            WETH9 wrappedNative,
-            LinkToken linkToken,
-            BurnMintERC677Helper ccipBnM,
-            BurnMintERC677Helper ccipLnM
-        ) = ccipLocalSimulator.configuration();
-
-
-        ccipLocalSimulator.requestLinkFromFaucet(receiver, amount);
-    }
-
-}
